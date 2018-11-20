f9ffb715c31b2102d95190ed9891b7b5d88c631a
mario-digitalstate <mario@digitalstate.ca>
2018-11-20 08:19:47 -0500

    Remove doctrine schema update from docker entrypoint

--- api/docker/php/docker-entrypoint.sh
+++ api/docker/php/docker-entrypoint.sh
@@ -17,7 +17,6 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
 		until pg_isready --timeout=0 --dbname="${DATABASE_URL}"; do
 			sleep 1
 		done
-		bin/console doctrine:schema:update --force --no-interaction
 	fi
 fi
 
