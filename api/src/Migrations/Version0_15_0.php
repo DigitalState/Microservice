5ec3c8bb4a887ec5b76542a45e71a48a25e9474f
mario-digitalstate <mario@digitalstate.ca>
2018-11-21 14:44:01 -0500

    Update migration

--- api/src/Migrations/Version0_15_0.php
+++ api/src/Migrations/Version0_15_0.php
@@ -4,6 +4,11 @@ namespace App\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+use Ds\Component\Acl\Migrations\Version0_15_0 as Acl;
+use Ds\Component\Config\Migrations\Version0_15_0 as Config;
+use Ds\Component\Metadata\Migrations\Version0_15_0 as Metadata;
+use Ds\Component\Parameter\Migrations\Version0_15_0 as Parameter;
+use Ds\Component\Tenant\Migrations\Version0_15_0 as Tenant;
 
 /**
  * Class Version0_15_0
@@ -17,14 +22,27 @@ final class Version0_15_0 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        $platform = $this->connection->getDatabasePlatform()->getName();
+        $acl = new Acl($this->version);
+        $acl->up($schema);
 
-        switch ($platform) {
+        $config = new Config($this->version);
+        $config->up($schema);
+
+        $metadata = new Metadata($this->version);
+        $metadata->up($schema);
+
+        $parameter = new Parameter($this->version);
+        $parameter->up($schema);
+
+        $tenant = new Tenant($this->version);
+        $tenant->up($schema);
+
+        switch ($this->platform) {
             case 'postgresql':
                 break;
 
             default:
-                $this->abortIf(true,'Migration cannot be executed on "'.$platform.'".');
+                $this->abortIf(true,'Migration cannot be executed on "'.$this->platform.'".');
                 break;
         }
     }
@@ -36,14 +54,27 @@ final class Version0_15_0 extends AbstractMigration
      */
     public function down(Schema $schema)
     {
-        $platform = $this->connection->getDatabasePlatform()->getName();
+        $acl = new Acl($this->version);
+        $acl->down($schema);
 
-        switch ($platform) {
+        $config = new Config($this->version);
+        $config->down($schema);
+
+        $metadata = new Metadata($this->version);
+        $metadata->down($schema);
+
+        $parameter = new Parameter($this->version);
+        $parameter->down($schema);
+
+        $tenant = new Tenant($this->version);
+        $tenant->down($schema);
+
+        switch ($this->platform) {
             case 'postgresql':
                 break;
 
             default:
-                $this->abortIf(true,'Migration cannot be executed on "'.$platform.'".');
+                $this->abortIf(true,'Migration cannot be executed on "'.$this->platform.'".');
                 break;
         }
     }
