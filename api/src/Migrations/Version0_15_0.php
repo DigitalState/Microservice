3105070865a842f3acf787b86a812a7e9d9fc686
mario-digitalstate <mario@digitalstate.ca>
2018-11-22 13:25:51 -0500

    Update migration

--- api/src/Migrations/Version0_15_0.php
+++ api/src/Migrations/Version0_15_0.php
@@ -4,17 +4,69 @@ namespace App\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+use Doctrine\DBAL\Migrations\Version;
 use Ds\Component\Acl\Migrations\Version0_15_0 as Acl;
 use Ds\Component\Config\Migrations\Version0_15_0 as Config;
+use Ds\Component\Container\Attribute;
+use Ds\Component\Database\Util\Objects;
+use Ds\Component\Database\Util\Parameters;
 use Ds\Component\Metadata\Migrations\Version0_15_0 as Metadata;
 use Ds\Component\Parameter\Migrations\Version0_15_0 as Parameter;
 use Ds\Component\Tenant\Migrations\Version0_15_0 as Tenant;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 
 /**
  * Class Version0_15_0
  */
-final class Version0_15_0 extends AbstractMigration
+final class Version0_15_0 extends AbstractMigration implements ContainerAwareInterface
 {
+    use Attribute\Container;
+
+    /**
+     * @cont string
+     */
+    const DIRECTORY = '/srv/api/config/migrations/0_15_0/';
+
+    /**
+     * @var \Ds\Component\Acl\Migrations\Version0_15_0
+     */
+    private $acl;
+
+    /**
+     * @var \Ds\Component\Config\Migrations\Version0_15_0
+     */
+    private $config;
+
+    /**
+     * @var \Ds\Component\Metadata\Migrations\Version0_15_0
+     */
+    private $metadata;
+
+    /**
+     * @var \Ds\Component\Parameter\Migrations\Version0_15_0
+     */
+    private $parameter;
+
+    /**
+     * @var \Ds\Component\Tenant\Migrations\Version0_15_0
+     */
+    private $tenant;
+
+    /**
+     * Constructor
+     *
+     * @param \Doctrine\DBAL\Migrations\Version  $version
+     */
+    public function __construct(Version $version)
+    {
+        parent::__construct($version);
+        $this->acl = new Acl($version);
+        $this->config = new Config($version);
+        $this->metadata = new Metadata($version);
+        $this->parameter = new Parameter($version);
+        $this->tenant = new Tenant($version);
+    }
+
     /**
      * Up migration
      *
@@ -22,27 +74,19 @@ final class Version0_15_0 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        $acl = new Acl($this->version);
-        $acl->up($schema);
+        $parameters = Parameters::parseFile(static::DIRECTORY.'parameters.yml');
+        $this->acl->up($schema, Objects::parseFile(static::DIRECTORY.'acl.yml', $parameters));
+        $this->config->up($schema);
+        $this->metadata->up($schema);
+        $this->parameter->up($schema);
+        $this->tenant->up($schema);
 
-        $config = new Config($this->version);
-        $config->up($schema);
-
-        $metadata = new Metadata($this->version);
-        $metadata->up($schema);
-
-        $parameter = new Parameter($this->version);
-        $parameter->up($schema);
-
-        $tenant = new Tenant($this->version);
-        $tenant->up($schema);
-
-        switch ($this->platform) {
+        switch ($this->platform->getName()) {
             case 'postgresql':
                 break;
 
             default:
-                $this->abortIf(true,'Migration cannot be executed on "'.$this->platform.'".');
+                $this->abortIf(true,'Migration cannot be executed on "'.$this->platform->getName().'".');
                 break;
         }
     }
@@ -54,27 +98,18 @@ final class Version0_15_0 extends AbstractMigration
      */
     public function down(Schema $schema)
     {
-        $acl = new Acl($this->version);
-        $acl->down($schema);
+        $this->acl->down($schema);
+        $this->config->down($schema);
+        $this->metadata->down($schema);
+        $this->parameter->down($schema);
+        $this->tenant->down($schema);
 
-        $config = new Config($this->version);
-        $config->down($schema);
-
-        $metadata = new Metadata($this->version);
-        $metadata->down($schema);
-
-        $parameter = new Parameter($this->version);
-        $parameter->down($schema);
-
-        $tenant = new Tenant($this->version);
-        $tenant->down($schema);
-
-        switch ($this->platform) {
+        switch ($this->platform->getName()) {
             case 'postgresql':
                 break;
 
             default:
-                $this->abortIf(true,'Migration cannot be executed on "'.$this->platform.'".');
+                $this->abortIf(true,'Migration cannot be executed on "'.$this->platform->getName().'".');
                 break;
         }
     }
